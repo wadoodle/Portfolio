e5b09d6614600dfeca891516045d3fdb974f2d30
Terrell Burton <lilmill64@yahoo.com>
2022-03-28 08:51:04 -0400

    added mobile nav animation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
+let clicks = 0;
 window.addEventListener('load', function () {
   const hamburger = document.getElementById('hamburger');
   const navigation = document.getElementById('nav');
+
   hamburger.addEventListener('click', function () {
+    clicks += 1;
     navigation.classList.toggle('active');
-    navigation.classList.toggle('inactive')
+    hamburger.classList.toggle('active');
+
+    if(clicks === 2) {
+      console.log('here');
+      navigation.classList.add('inactive');
+      hamburger.classList.add('inactive');
+    } else if(clicks > 2) {
+      navigation.classList.toggle('inactive');
+      hamburger.classList.toggle('inactive');
+    }
+
   });
 })
 
